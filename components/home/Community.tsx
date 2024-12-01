239cd91a722c877e93ee6a836d0713811654c60e
Varun Deva <varundev23@gmail.com>
2024-12-01 15:14:07 +0530

    fix: additional padding

--- components/home/Community.tsx
+++ components/home/Community.tsx
@@ -12,9 +12,9 @@ export const Community = () => {
   return (
     <div className="py-3 ">
       <hr className="border-secondary" />
-      <div className="container py-4 sm:py-5">
+      <div className="py-4 sm:py-5">
         <div className="mx-auto lg:w-3/5">
-          <Card className="flex flex-col items-center justify-center border-none bg-background text-center shadow-none">
+          <Card className="flex flex-col items-center  justify-center border-none bg-background p-0 text-center shadow-none">
             <CardHeader>
               <CardTitle className="flex flex-col items-center text-4xl font-bold md:text-5xl">
                 <Icons.discord />
